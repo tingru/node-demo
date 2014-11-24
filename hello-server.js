b2a7e67df9915360914d8ebc468d5a65ffdb0659
T.Guo <guotingru@outlook.com>
2014-11-24 12:22:39 -0800

    Update hello-server.js
    class change

--- hello-server.js
+++ hello-server.js
@@ -6,3 +6,32 @@
 
 'use strict';
 
+/*var http = require('http');
+
+var server = http.createServer(function(req, res) {
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+   res.end('Hello World!\n');
+});
+
+server.listen(8080, function() {
+   console.log('server is listening at http://localhost:8080')
+});*/
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World!');
+});
+app.get('/foo', function (req, res) {
+    res.send('You requested foo!');
+});
+
+app.post('/foo', function(req, res) {
+
+});
+
+app.listen(8080, function () {
+    console.log('listening at http://localhost:8080');
+});
+
